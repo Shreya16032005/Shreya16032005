1860c6d227fc9ac8dc72f92b04b9aeda7d3c12c0
Shreya16032005 <shreya16032005@gmail.com>
2024-07-31 21:21:04 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+class Hello{
+public static void main(String[] args){
+String a,b,c;
+int x,y,z;
+a=args[0];
+b=args[1];
+x=Integer.parseInt(a);
+y=Integer.parseInt(b);
+z=x+y;
+System.out.println("The sum of number="+z);
+    }
+}
