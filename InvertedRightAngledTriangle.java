179240ce00adcb4ba574bdc64df379f82f782288
Shreya16032005 <shreya16032005@gmail.com>
2024-08-07 20:04:05 +0530

    Create InvertedRightAngledTriangle.java

---
+++ InvertedRightAngledTriangle.java
@@ -0,0 +1,11 @@
+class InvertedRightAngledTriangle{
+public static void main(String[] args){
+int n=5;
+for(int i=n.java;i>=1;i--){
+for(int j=1;j<=i;j++){
+System.out.println("*");
+}
+System.out.println();
+}
+}
+}
